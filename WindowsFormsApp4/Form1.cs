2253664be18c62634ff4045ae18c5e5e911e9003
TheAtrial <38584196+TheAtrial@users.noreply.github.com>
2022-08-13 18:23:18 +0300

    Add files via upload

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        System.IO.FileInfo fi;
+        OpenFileDialog Fd = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Fd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            //saveOpenFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            List<string> text1 = Mas();
+
+            string []text = text1.ToArray();
+            Array.Sort(text);
+
+            if (text.Length == 0)
+            {
+                MessageBox.Show("Файл пуст");
+            }
+            else
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    listBox1.Items.Add(text[i]);
+                }
+                MessageBox.Show("Файл открыт");
+            }
+
+
+        }
+
+        public List<string> Mas()
+        {
+            List<string> text = new List<string>();
+            if (Fd.ShowDialog() == DialogResult.Cancel)
+            {
+                return null;
+            }
+            // получаем выбранный файл
+            string filename = Fd.FileName;
+            // читаем файл в строку
+            string[] fileText = System.IO.File.ReadAllLines(filename, Encoding.GetEncoding(1251));
+            for (int i = 0; i <fileText.Length; i++)
+            {
+                text.Add(fileText[i]); 
+            }
+            return text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] text;
+            text = new string[listBox1.Items.Count];
+            
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                text[i] = listBox1.Items[i].ToString();
+            }
+            string main = text[0];
+            string tmp;
+            int o = 0;
+            int c = 0;
+            char jk;
+            int jj = 0;
+            string[] text1 = new string[3];
+            int p = 0;
+            int rez = 0;
+            string word;
+                for (int l = 1; l < listBox1.Items.Count; l++)
+                {
+                    tmp = text[1];
+                    string tmp1 = text[1];
+                    while (tmp.Length != 0)
+                    {
+                        rez = main.IndexOf(tmp);
+                        if (rez >= 0)
+                        {
+                        
+                        if ((rez - main.Length) * (-1) == tmp.Length )
+                            {
+                                p = tmp.Length;
+                                tmp1 = tmp1.Substring(p);
+                                word = main + tmp1;
+                                MessageBox.Show(word);
+                                break;
+                            }
+                            else
+                            {
+                            tmp = tmp.Substring(0, tmp.Length - 1);
+                            MessageBox.Show(tmp.Length.ToString());
+                            }
+                        }
+                        else if (rez == -1 && tmp.Length == 1)
+                        {
+
+                        word = main + tmp1;
+                        MessageBox.Show(word);
+                        break;
+                        //MessageBox.Show(rez.ToString());
+                        }
+                        else if (rez == -1)
+                        {
+                           
+                             tmp = tmp.Substring(0, tmp.Length-1);
+                             word = ("no");
+                            MessageBox.Show(tmp.Length.ToString());
+                        }
+                        
+                }
+                
+                }
+            
+
+
+
+        }
+    }
+}
---
+++ WindowsFormsApp4/oprog.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class oprog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(66, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Программа по поиску слова \r\nв справочнике с использованием\r\n бинарного метода пои" +
+    "ска";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(113, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Автор: D\r\nГруппа D";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(197, 416);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "2021";
+            // 
+            // oprog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "oprog";
+            this.Text = "О программе";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
